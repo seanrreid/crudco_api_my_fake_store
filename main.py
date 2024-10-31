26334f0aa20e01c64a4c7568de1b0379b511e74e
Sean R Reid <sean@torchcodelab.com>
2024-10-31 09:03:59 -0400

    Added POSt and single Product route

--- main.py
+++ main.py
@@ -65,6 +65,38 @@ async def get_products(session: Session = Depends(get_session)):
     return products_list
 
 
+@app.get('/products/{id}')
+async def get_sing_product(id: int, session: Session = Depends(get_session)):
+    statement = (
+        select(Product, Category, Brand)
+        .where(Product.id == id)
+        .join(Category, Category.id == Product.category_id)
+        .join(Brand, Brand.id == Product.brand_id)
+    )
+    result = session.exec(statement).first()
+
+    product, category, brand = result
+
+    product_details = {
+        "id": product.id,
+        "brand": brand.name,
+        "title": f"{brand.name} {product.title}",
+        "image": f"{BASE_URL}/static/images/{product.image}",
+        "description": product.description,
+        "category": category.name,
+        "price": product.price
+    }
+
+    return product_details
+
+
+@app.post('/products/add')
+async def add_product(request: Product, session: Session = Depends(get_session)):
+    session.add(request)
+    session.commit()
+    return {"Product Added": request.title}
+
+
 @app.get('/categories')
 async def get_categories(session: Session = Depends(get_session)):
     statement = select(Category)
