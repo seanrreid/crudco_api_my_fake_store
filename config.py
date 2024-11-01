8403f36120f342f2f633d012a7fa29e6d8b735b4
Sean R Reid <sean@torchcodelab.com>
2024-11-01 14:54:40 -0400

    Updated config, names are dumb

--- config.py
+++ config.py
@@ -5,11 +5,7 @@ from pathlib import Path
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
-SUPABASE_DB_URL = os.getenv("SUPABASE_DB_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 SUPABASE_SECRET_KEY = os.getenv("SUPABASE_SECRET_KEY")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
 
